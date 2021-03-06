1d5fe8dafe8554bac7cd10463008d206eafeb616
Augusto Madeira Splett <augusto.splett@gmail.com>
2021-03-05 23:59:02 -0300

    final commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,34 @@
+//Elements
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+//Dinosaur Functions
+let isJumping = false;
+let position = 0
 
 function handleKeyUp(event){
     //console.log(event.keyCode);
     if(event.keyCode === 32){
-        jump();
+        if(!isJumping){
+            jump();
+        }  
     }
 }
 
 function jump(){
-    let position = 0
+    
+    isJumping = true;
     let upInterval = setInterval(() => {
         if(position >=150){
             clearInterval(upInterval);
+
             let downInterval = setInterval(() =>{
                 if(position <=0){
                     clearInterval(downInterval);
+                    isJumping = false;
                 }else{
                     position-=20;
                     dino.style.bottom = position + 'px';
+                    
                 }
             }, 20);
         }else{
@@ -29,4 +40,30 @@ function jump(){
     }, 20);
 }
 
+//Cactus Functions
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(()=>{
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>'
+        }else{
+            cactusPosition -=10;
+            cactus.style.left = cactusPosition + 'px'
+        }
+    },20);
+    setTimeout(createCactus, randomTime);
+};
+createCactus();
+//Listners
 document.addEventListener('keyup', handleKeyUp);
